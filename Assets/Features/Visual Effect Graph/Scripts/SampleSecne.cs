0b1d931db2fea7d71dae8f531af9af9b8d95057f
fazelahmadvand <fazelahmadvand@gmail.com>
2023-12-29 14:27:38 +0330

    visual effect graph

---
+++ Assets/Features/Visual Effect Graph/Scripts/SampleSecne.cs
@@ -0,0 +1,26 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+
+public class SampleSecne : MonoBehaviour
+{
+
+    [SerializeField] private VisualEffect vfx;
+    [SerializeField] private AudioSource vfxSound;
+    [SerializeField] private float duration = .9f;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            vfxSound.DOFade(0, duration);
+            vfx.Stop();
+        }
+    }
+
+
+}
